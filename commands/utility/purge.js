cc65ad62c3d12c7197491b61148ba09d2b3f714d
zleodai <awsomeleodai@gmail.com>
2025-12-18 03:24:31 -0800

    \almost done

---
+++ commands/utility/purge.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { purgeText } = require('../../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Purges')
+        .addIntegerOption((option) => option.setName('depth').setDescription('How much messages should I check to purge (rounded to nearest hundred)').setRequired(true)),
+
+	async execute(interaction) {
+        const depth = interaction.options.getInteger('depth');
+
+        const channels = await interaction.member.guild.channels;
+        const channel = await channels.fetch(interaction.channelId);
+        let messages = await channel.messages.fetch({limit: 100});
+
+        let lastMsg = null;
+        let purgeCount = 0;
+        for (let i = 0; i < Math.floor(depth/100) +1; i++) {
+            const messagesToPurge = []
+
+            messages.forEach(msg => {
+                const text = msg.content;
+
+                let badText = false;
+                purgeText.forEach(badString => {
+                    badText = badText || text.includes(badString)
+                })
+
+                if (badText) {
+                    console.log(`Purging ${msg.author.tag}: ${msg.content}`);
+
+                    purgeCount++;
+
+                    messagesToPurge.push(msg.id)
+                }
+
+                lastMsg = msg;
+            });
+
+            messages = await channel.messages.fetch({before: lastMsg.id, limit: 100})
+        }
+
+        console.log(`Purged ${purgeCount} messages`);
+
+		await interaction.reply({ content: "Purging ...", ephemeral: true});
+	},
+};
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ] 
+});
 
 client.commands = new Collection();
 
@@ -16,7 +22,6 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -25,34 +30,16 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-})
-
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return; 
-    
-    const command = interaction.client.commands.get(interaction.commandName);
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
-		} else {
-			await interaction.reply({
-				content: 'There was an error while executing this command!',
-				flags: MessageFlags.Ephemeral,
-			});
-		}
-	}
-})
+}
 
 client.login(token);
